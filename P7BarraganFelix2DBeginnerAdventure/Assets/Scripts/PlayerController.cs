e5853a63d23d920090fd4321e6afa89d722067ad
728843-ui <728843@student.dusd.net>
2025-11-05 15:27:06 -0800

    bruh

--- P7BarraganFelix2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P7BarraganFelix2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DuckoController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,7 +14,7 @@ public class DuckoController : MonoBehaviour
     void Update()
     {
         Vector2 position = transform.position;
-        position.x = position.x = 0.1f;
+        position.x = position.x + 0.1f;
         transform.position = position;
     }
 }
