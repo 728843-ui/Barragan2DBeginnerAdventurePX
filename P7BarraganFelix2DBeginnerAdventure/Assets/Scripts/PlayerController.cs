c5665c541589226aa55650638d8d26ed728c4053
728843-ui <728843@student.dusd.net>
2025-12-03 17:26:01 -0800

    scripting stuff

--- P7BarraganFelix2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P7BarraganFelix2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -4,28 +4,60 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public int maxHealth = 5;
+    public float timeInvincible = 2;
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
-
+    public float speed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
-      rigidbody2d = GetComponent<Rigidbody2D>();
-
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-         horizontal = Input.GetAxis("Horizontal");
-         vertical = Input.GetAxis("Vertical");
-       
-       Vector2 position = transform.position;
-       position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-       position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+            {
+                isInvincible = false;
+            }
+        }
+        Vector2 position = transform.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
 
         rigidbody2d.MovePosition(position);
+    }
+   public void ChangeHealth(int amount)
+    {
+        if(amount < 0)
+        {
+            if(isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
