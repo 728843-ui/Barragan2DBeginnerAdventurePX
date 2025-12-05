584ad5cc0eae9080fa3cf27b9b40dc264c165e6b
728843-ui <728843@student.dusd.net>
2025-12-04 22:23:14 -0800

    made animations les go

--- P7BarraganFelix2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P7BarraganFelix2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
 
     public int maxHealth = 5;
-    public float timeInvincible = 2;
+    public float timeInvincible = 2.0F;
     public int health { get { return currentHealth; } }
     int currentHealth;
 
@@ -17,11 +17,15 @@ public class PlayerController : MonoBehaviour
     float horizontal;
     float vertical;
     float speed = 10.0f;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
         
     }
 
@@ -30,6 +34,18 @@ public class PlayerController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f ) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
 
         if (isInvincible)
         {
@@ -50,6 +66,8 @@ public class PlayerController : MonoBehaviour
     {
         if(amount < 0)
         {
+            animator.SetTrigger("Hit");
+
             if(isInvincible)
             {
                 return;
